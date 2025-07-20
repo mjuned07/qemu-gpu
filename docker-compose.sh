5c609b656cd2328343842bb338172d67f9ce6b19
mominjuned <momimjuned47@gmail.com>
2025-07-20 16:15:38 +0530

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -7,20 +7,8 @@ ngrok config add-authtoken $CRP
 mkdir -p windows && cd windows
 wget https://raw.githubusercontent.com/mjuned07/qemu-gpu/refs/heads/main/compose.yml
 
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+pip install udocker
+udocker --allow-root install
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-docker compose up -d
+udocker compose up -d
 ngrok start --all --log=stdout

713683098dd183057e4ad1977710300260f61ed6
mominjuned <momimjuned47@gmail.com>
2025-07-20 12:39:19 +0530

    Create docker-compose.sh

---
+++ docker-compose.sh
@@ -0,0 +1,10 @@
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar -xvzf ~/Downloads/ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+mkdir -p ~/.config/ngrok
+wget https://raw.githubusercontent.com/mjuned07/qemu-gpu/refs/heads/main/ngrok.yml -O ~/.config/ngrok/ngrok.yml
+read -p "Paste authtoken here: " CRP
+ngrok config add-authtoken $CRP
+mkdir -p windows && cd windows
+wget https://raw.githubusercontent.com/mjuned07/qemu-gpu/refs/heads/main/compose.yml
+docker compose up -d
+ngrok start --all --log=stdout
